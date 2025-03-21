79e31c0f4a78529b6296258f9781f605fbe09f79
YoonMooYeol <agecross@naver.com>
2025-03-21 15:47:24 +0900

    로그인 페이지에서 구글 로그인 버튼 제거

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -360,23 +360,6 @@ const goToFindPassword = () => {
 
             <div class="flex flex-col space-y-3">
               <!-- 소셜 로그인 버튼 -->
-              <button
-                type="button"
-                class="flex items-center justify-center w-full px-4 py-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-[20px] shadow-sm hover:shadow-md transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-point-yellow focus:ring-opacity-50"
-                @click="initiateGoogleLogin"
-              >
-                <svg
-                  class="w-5 h-5 mr-3"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"
-                    fill="#4285F4"
-                  />
-                </svg>
-                <span>Google 계정으로 로그인</span>
-              </button>
 
               <button
                 type="button"
