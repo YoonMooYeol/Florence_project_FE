72ca37b0cc2bf5c6b2ee0c7e70e475ad1e46f15c
YoonMooYeol <agecross@naver.com>
2025-03-27 09:04:15 +0900

    프로필 페이지 임신 정보 표시 조건 수정
    - 사용자 정보에서 임신 여부에 따라 표시되는 제목과 내용을 수정하였습니다.
    - 이제 활성 상태와 관계없이 임신 정보가 있는 경우에만 해당 내용이 표시됩니다.

--- src/views/user/Profile.vue
+++ src/views/user/Profile.vue
@@ -275,13 +275,13 @@ const test =  () => {
         <div class="bg-white rounded-lg shadow-md p-6 mb-4">
           <div class="flex justify-between items-center mb-4">
             <!-- 임신 정보가 있을 때만 표시 -->
-            <h2 v-if="userInfo.isPregnant && userInfo.isActive" class="text-lg font-bold text-dark-gray">
+            <h2 v-if="userInfo.isPregnant" class="text-lg font-bold text-dark-gray">
               ♥︎사랑스런 {{ userInfo.babyNickname }}{{ getJosa(userInfo.babyNickname, '과', '와') }} 만나기까지♥︎
             </h2>
           </div>
 
           <div
-            v-if="userInfo.isPregnant && userInfo.isActive"
+            v-if="userInfo.isPregnant"
             class="space-y-4"
           >
             <div class="flex justify-between items-center">
